b4e0154aa0eb2bb7785b0898a1232ba4ec56fda1
Nathan <antnefedov33@gmail.com>
2025-06-23 17:11:06 +0300

    prettier installed and configured

--- src/app/types/vite-env.d.ts
+++ src/app/types/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-	import { DefineComponent } from 'vue';
+    import { DefineComponent } from 'vue';
 
-	const component: DefineComponent<object, object, unknown>;
-	export default component;
+    const component: DefineComponent<object, object, unknown>;
+    export default component;
 }
